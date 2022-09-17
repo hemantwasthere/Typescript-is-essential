8749955c30c0e3be8d54cce2af9a952746ef6b05
Hemant <hrsmart2150@gmail.com>
2022-09-18 01:38:49 +0530

    Saw useRef hook types for MutableRef

--- react-typescript/src/components/ref/MutableRef.tsx
+++ react-typescript/src/components/ref/MutableRef.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function MutableRef() {
+    const [timer, setTimer] = useState(0)
+    const interValRef = useRef<null | number>(null)
+
+    const stopTimer = () => {
+        interValRef.current && window.clearInterval(interValRef.current)
+    }
+
+    useEffect(() => {
+        // setInterval returns a number, which is the id of the interval
+        interValRef.current = window.setInterval(() => {
+            setTimer((timer) => timer + 1)
+        }, 1000)
+        return () => {
+            stopTimer()
+        }
+    }, [])
+
     return (
-        <div>MutableRef</div>
+        <div>
+            HookTimer = {timer} -
+            <button onClick={() => stopTimer()}>Stop Timer</button>
+        </div>
     )
 }
 
