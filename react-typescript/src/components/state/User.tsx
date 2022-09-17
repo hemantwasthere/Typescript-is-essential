9eac11233fe396eccc3a2c7fb66078ea70dcdfff
Hemant <hrsmart2150@gmail.com>
2022-09-17 19:59:00 +0530

    Saw type assertion in useState hook

--- react-typescript/src/components/state/User.tsx
+++ react-typescript/src/components/state/User.tsx
@@ -6,18 +6,20 @@ type AuthUser = {
 }
 
 function User() {
-    const [user, setUser] = useState<null | AuthUser>(null) // defining the type of usestate that it could be of null or AuthUser
+    // const [user, setUser] = useState<null | AuthUser>(null) // defining the type of usestate that it could be of null or AuthUser
 
-    const handleLogin = () => {
-        setUser({ name: 'Hemant', email: 'hello@itsmehemant.tech' })
-    }
-    const handleLogout = () => {
-        setUser(null)
-    }
+    const [user, setUser] = useState<AuthUser>({} as AuthUser) // type assertion - if we are sure that user value will be provided before component mounts, then we can use type assertion. for eg. like we sets login state in useEffect hook
+
+    // const handleLogin = () => {
+    //     setUser({ name: 'Hemant', email: 'hello@itsmehemant.tech' })
+    // }
+    // const handleLogout = () => {
+    //     setUser(null)
+    // }
     return (
         <div>
-            <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
+            {/* <button onClick={handleLogin}>Login</button> */}
+            {/* <button onClick={handleLogout}>Logout</button> */}
             <div>User name is {user?.name}</div>
             <div>User email is {user?.email}</div>
         </div>
